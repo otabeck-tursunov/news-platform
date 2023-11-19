5f59888b06682590c45f23083b4ab3b581dc2d26
otabek-tursunov <tursunovotabekkuva@gmail.com>
2023-11-19 12:36:18 +0500

    context_processor.py via settings.py templates

--- newsApp/urls.py
+++ newsApp/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
-from .views import news_list, news_detail, homePageView, ContactPageView
+from .views import news_list, news_detail, HomePageView, ContactPageView
 
 urlpatterns = [
-    path('', homePageView, name='home-page'),
+    path('', HomePageView.as_view(), name='home-page'),
     path('news/', news_list, name="news-list"),
     path('<int:id>/', news_detail, name="news-detail"),
     path('contact-us/', ContactPageView.as_view(), name="contact-page"),
--- newsApp/views.py
+++ newsApp/views.py
@@ -1,6 +1,6 @@
 from django.http import HttpResponse
 from django.shortcuts import render, get_object_or_404, redirect
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView, ListView
 
 from .models import Category, News
 from .forms import ContactForm
@@ -23,27 +23,31 @@ def news_detail(request, id):
 
 
 def homePageView(request):
-    news = News.published.all()
     categories = Category.objects.all()
+    news_list = News.published.all().order_by('-publish_time')[:10]
+    local_news = News.published.filter(category__name='Mahalliy').order_by('-publish_time')[:6]
     context = {
-        'news': news,
-        'categories': categories
+        'news_list': news_list,
+        'categories': categories,
+        'local_news': local_news
     }
     return render(request, 'news/index.html', context)
 
+class HomePageView(ListView):
+    model = News
+    template_name = 'news/index.html'
+    context_object_name = 'news'
 
-# def contactPageView(request):
-#     form = ContactForm(request.POST or None)
-#     if request.method == 'POST' and form.is_valid():
-#         form.save()
-#         return redirect('contact-page')
-#     context = {
-#         'form': form
-#     }
-#     return render(request, 'news/contact.html', context)
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['categories'] = self.model.objects.all()
+        context['news_list'] = News.published.all().order_by('-publish_time')[:10]
+        context['local_news'] = News.published.filter(category__name='Mahalliy').order_by('-publish_time')[:6]
+        return context
 
 class ContactPageView(TemplateView):
     template_name = 'news/contact.html'
+
     def get(self, request, *args, **kwargs):
         form = ContactForm()
         context = {
